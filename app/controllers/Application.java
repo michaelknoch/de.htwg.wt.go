949cae9c2968c94ada5252a79eb2d5573c49b4e2
Michael Knoch <info@michaelknoch.de>
2014-10-08 14:45:52 +0200

    import gocontroller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,9 +2,11 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import de.htwg.go.Go;
+import de.htwg.go.controller.IGoController;
 
 public class Application extends Controller {
-    
+
     public static Result index() {
         return ok(views.html.index.render("Hello Play Framework"));
     }
