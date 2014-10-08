d96f29cca9a914c452adfab62909f9aaffce6a7d
Michael Knoch <info@michaelknoch.de>
2014-10-08 15:49:20 +0200

    controller jar connection

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,9 +6,10 @@ import de.htwg.go.Go;
 import de.htwg.go.controller.IGoController;
 
 public class Application extends Controller {
+    static IGoController controller = Go.getInstance().getController();
 
     public static Result index() {
-        return ok(views.html.index.render("Hello Play Framework"));
+
+        return ok(views.html.index.render(controller.getStatus()));
     }
-    
 }
