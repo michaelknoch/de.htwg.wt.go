257d5adf4e43007c310b2bb8c04d416af911149d
Timo Weiß <info@timo-weiss.com>
2014-10-22 10:58:56 +0200

    add mainCtrljs, add vendor/angular.min.js

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -15,7 +15,6 @@ public class Application extends Controller {
     static IGoController controller = Go.getInstance().getController();
 
     public static Result index() {
-
         return ok(views.html.index.render(controller.getStatus()));
     }
 
