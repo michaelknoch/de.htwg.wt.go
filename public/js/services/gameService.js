9a4582de508d935bfda41c9b597e202a09721972
Timo Weiß <info@timo-weiss.com>
2014-10-22 13:53:15 +0200

    stuff

--- public/js/services/gameService.js
+++ public/js/services/gameService.js
@@ -5,6 +5,9 @@ angular.module('goApp')
 .service('GameService', function($http, $q) {
 
     return {
+        getStatus: function() {
+            return $http.get('/getStatus');
+        },
         setStone: function(formData) {
             return $http.post('/setStone', formData);
         }
