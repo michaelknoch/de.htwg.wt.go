faabf8a05634f30ca5de9535066e35a08c697887
Timo Weiß <info@timo-weiss.com>
2014-10-22 15:11:26 +0200

    gamefield

--- public/js/ctrl/mainCtrl.js
+++ public/js/ctrl/mainCtrl.js
@@ -11,12 +11,14 @@ angular.module('goApp')
     .controller('MainCtrl', function($scope, $rootScope, $state, GameService) {
         console.log(GameService);
         $scope.errorState = false;
+        $scope.gameField = [];
+
         $scope.setStone = function() {
             var json = {
                 x: 1,
                 y: 2
             };
-            var promise = GameService.setStone(JSON.stringify(json));
+            var promise = GameService.getGameField();
             promise.error(function(resp) {
                 $scope.errorState = true;
                 $scope.setStoneState = resp;
@@ -27,4 +29,14 @@ angular.module('goApp')
         };
         $rootScope.headerTitle = 'Übersicht';
         $scope.test = 'asdasdasdTest';
+
+        $scope.getGameField = function() {
+            var promise = GameService.getGameField();
+            promise.then(function(resp) {
+                $scope.gameField = resp.data.gamefield;
+            });
+        };
+
+        //bootstrap gamefield
+        $scope.getGameField();
     });
