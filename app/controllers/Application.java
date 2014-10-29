63f1edd28b654998b0f9509573e5167df98d42bc
Timo Weiß <info@timo-weiss.com>
2014-10-29 14:06:30 +0100

    remove margin

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-
 import play.libs.Json;
 import com.fasterxml.jackson.databind.JsonNode;
 
