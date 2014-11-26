0ecff7bd44bb769b958b1d82ba458b713c0d833a
Michael Knoch <info@michaelknoch.de>
2014-11-26 14:26:51 +0100

    add websocket

---
+++ app/model/GameFieldObserver.java
@@ -0,0 +1,27 @@
+package model;
+
+import play.mvc.WebSocket;
+import play.mvc.WebSocket.Out;
+import de.htwg.go.controller.IGoController;
+import de.htwg.go.util.observer.Event;
+import de.htwg.go.util.observer.IObserver;
+
+public class GameFieldObserver implements IObserver {
+
+    private Out<String> out;
+    private IGoController controller;
+
+
+    public GameFieldObserver(IGoController controller,WebSocket.Out<String> out) {
+        controller.addObserver(this);
+        this.controller = controller;
+        this.out = out;
+    }
+
+    @Override
+    public void update(Event arg0) {
+        out.write(controller.getNext());
+        System.out.println("WUI was updated");
+    }
+
+}
