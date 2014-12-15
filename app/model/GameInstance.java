9dd889c1717d147c96458730377d9f3218651fe8
Timo Weiß <info@timo-weiss.com>
2014-12-15 19:20:04 +0100

    add getter, add hashCode as id

--- app/model/GameInstance.java
+++ app/model/GameInstance.java
@@ -9,15 +9,24 @@ public class GameInstance {
     private String player1;
     private String player2;
     private GoController controller;
+    public final int gameId;
 
     public GameInstance(String name, GoController controller) {
         this.player1 = name;
         this.player2 = "anonym";
         this.controller = controller;
+        this.gameId = controller.hashCode();
     }
 
     public void setPlayer2(String name) {
         this.player2 = name;
+    }
+
+    public String getPlayer1() {
+        return this.player1;
+    }
+    public String getPlayer2() {
+        return this.player2;
     }
 
 
