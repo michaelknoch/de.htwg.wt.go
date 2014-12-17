4e04b7b5b7e107b3b775db19c9c1e41f34e4f71e
Michael Knoch <info@michaelknoch.de>
2014-12-17 13:33:48 +0100

    responsive scaling

--- public/js/directives/scale.js
+++ public/js/directives/scale.js
@@ -1,9 +1,17 @@
 
 angular.module('goApp').directive('scale', function() {
   return function(scope, element, attrs) {
-    var wrapperWidth = $('.field-wrapper').width() - 30;
-    var value = (wrapperWidth / 9);
-    angular.element(element).css('width', value + 'px');
-    angular.element(element).css('height', value + 'px');
-  };
-})
+        $(window ).resize(function() {
+            scale();
+        });
+
+        function scale () {
+            var wrapperWidth = $('.field-wrapper').width() - 30;
+            var value = (wrapperWidth / scope.gameField.length);
+            angular.element(element).css('width', value + 'px');
+            angular.element(element).css('height', value + 'px');
+        }
+    scale();
+  }
+
+});
