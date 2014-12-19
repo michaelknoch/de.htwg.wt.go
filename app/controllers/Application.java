54bcf5dd7cd345e1af0960abfa242cf4b9073ce9
Timo Weiß <info@timo-weiss.com>
2014-12-19 22:20:19 +0100

    refactor websockets

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,6 +18,7 @@ import java.util.*;
 
 public class Application extends Controller {
     static Map<Integer, GameInstance> gameInstances = new HashMap<Integer, GameInstance>();
+    static Map<Integer, WebSocket> sockets = new HashMap<Integer, WebSocket>();
 
     public static Result index() {
         System.out.println("new client");
@@ -164,8 +165,8 @@ public class Application extends Controller {
         return ok(gameInstance.getController().getOperate() + "");
     }
 
-    public static WebSocket<String> connectWebSocket() {
-        return new WebSocket<String>() {
+    public static WebSocket<String> connectWebSocket(int gameId) {
+        WebSocket ws = new WebSocket<String>() {
             int gameId = Integer.parseInt(session("gameId"));
             GameInstance gameInstance = gameInstances.get(gameId);
             public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
@@ -173,6 +174,8 @@ public class Application extends Controller {
             }
 
         };
+        sockets.put(gameId, ws);
+        return ws;
 
     }
 
