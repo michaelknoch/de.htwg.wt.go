be9317763913c717a278104e6f518d80fcb4dbb8
Timo Weiß <info@timo-weiss.com>
2015-01-14 23:24:09 +0100

    refactor loading directive

--- public/js/directives/loading.js
+++ public/js/directives/loading.js
@@ -1,10 +1,23 @@
 angular.module('goApp').directive('loading', function($timeout) {
-    return function(scope, element, attrs) {
+    // return function(scope, element, attrs) {
 
-        $timeout(function(){
-           $('.game-wrapper').addClass('show');
-           $('.spin-wrapper').addClass('hide');
-        }, 1600);
+    //     $timeout(function() {
+    //         $('.game-wrapper').addClass('show');
+    //         $('.spin-wrapper').addClass('hide');
+    //     }, 1600);
+    // };
+
+    return {
+        restrict: 'E', // Element <scoreboard></scoreboard>
+        scope: {},
+        templateUrl: 'assets/partials/loading.html',
+        link: function($scope, element, attrs) {
+            setTimeout(function() {
+                $('.game-wrapper').addClass('show');
+                $('.spin-wrapper').addClass('hide');
+            }, 1600);
+        }
+
     };
 
 });
