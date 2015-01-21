f5d9c91251839dee014fe511d8efd1cd2360cda1
Timo Weiß <info@timo-weiss.com>
2015-01-21 01:58:04 +0100

    remove unused code

--- public/test/test.js
+++ public/test/test.js
@@ -49,45 +49,4 @@ describe("Unit: Testing Controllers", function() {
         expect(controller).not.toBe(null);
     });
 
-
-    // it('should have a properly working VideosCtrl controller', inject(function($rootScope, $controller, $httpBackend) {
-    //     var searchTestAtr = 'cars';
-    //     var response = $httpBackend.expectJSONP(
-    //         'https://gdata.youtube.com/feeds/api/videos?q=' + searchTestAtr + '&v=2&alt=json&callback=JSON_CALLBACK');
-    //     response.respond(null);
-
-    //     var $scope = $rootScope.$new();
-    //     var ctrl = $controller('VideosCtrl', {
-    //         $scope: $scope,
-    //         $routeParams: {
-    //             q: searchTestAtr
-    //         }
-    //     });
-    // }));
-
-    // it('should have a properly working VideoCtrl controller', inject(function($rootScope, $controller, $httpBackend) {
-    //     var searchID = 'cars';
-    //     var response = $httpBackend.expectJSONP(
-    //         'https://gdata.youtube.com/feeds/api/videos/' + searchID + '?v=2&alt=json&callback=JSON_CALLBACK');
-    //     response.respond(null);
-
-    //     var $scope = $rootScope.$new();
-    //     var ctrl = $controller('VideoCtrl', {
-    //         $scope: $scope,
-    //         $routeParams: {
-    //             id: searchID
-    //         }
-    //     });
-    // }));
-
-    // it('should have a properly working WatchedVideosCtrl controller', inject(function($rootScope, $controller, $httpBackend) {
-    //     var $scope = $rootScope.$new();
-
-    //     //we're stubbing the onReady event
-    //     $scope.onReady = function() {};
-    //     var ctrl = $controller('WatchedVideosCtrl', {
-    //         $scope: $scope
-    //     });
-    // }));
-
 });
